236f39793572fcf32ba0f5dcffa8eacbe410e610
LENOVO <LENOVO@DESKTOP-2L9NM0M>
2021-02-17 19:06:34 +0530

    pushing to te1 branch only

--- src/gitPractise/Practise.java
+++ src/gitPractise/Practise.java
@@ -5,6 +5,7 @@ public class Practise {
 	public static void main(String[] args) {
 		System.out.println("normal practising");
 		System.out.println(" practising hard ");
+		System.out.println("push to te1 branch");
 	}
 
 }
